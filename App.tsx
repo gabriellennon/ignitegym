487515d4910668ea9d5e034cffcd5c8705ace23f
gabriel <gabriellennon7@gmail.com>
2023-04-05 17:45:32 -0300

    signIn screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme'
+import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
 
   return (
-    <View>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View />}
-    </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
