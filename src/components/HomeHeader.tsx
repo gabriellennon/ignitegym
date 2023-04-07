cb9816bc7e9a1aff156844288c33809a0ff9a781
gabriel <gabriellennon7@gmail.com>
2023-04-07 14:05:33 -0300

    group component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,33 @@
+import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons';
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader(){
+    return(
+        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto  
+                source={{ uri: 'https://github.com/gabriellennon.png' }}
+                size={16}
+                alt="Imagem do usuário"
+                mr={4}
+            />
+            <VStack flex={1}>
+                <Text color="gray.100" fontSize="md">
+                    Olá
+                </Text>
+                <Heading color="gray.100" fontSize="md">
+                    Gabriel
+                </Heading>
+            </VStack>
+            <TouchableOpacity>
+                <Icon 
+                    as={MaterialIcons}
+                    name="logout" 
+                    color="gray.200"
+                    size={7}
+                />
+            </TouchableOpacity>
+        </HStack>
+    )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,44 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 export function Home(){
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro' ]);
+    const [groupSelected, setGroupSelected] = useState('costa');
+
     return(
-        <Center flex={1}>
-            <Text color="white">Home</Text>
-        </Center>
+        <VStack flex={1}>
+            <HomeHeader />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group 
+                        name={item}
+                        isActive={groupSelected === item} 
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxHeight={10}
+            />
+
+            <VStack flex={1} px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+                    <Text color="gray.200" fontSize="sm">
+                        4
+                    </Text>
+                </HStack>
+                
+            </VStack>
+        </VStack>
     )
 }
